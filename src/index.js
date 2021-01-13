d45aef570099392581c018932ec18511de142344
Ultras <ultras-sur@mail.ru>
2021-01-13 22:11:35 +0300

    create brain-gcd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import readlineSync from 'readline-sync';
-// Получение опретора вычисления
-const getOperator = () => randomArrayElement(['*', '+', '-']);
-
-// Получение имени пользователя
-const getNameGlobal = () => readlineSync.question('May I have your name? ');
 
 // Получение случайного элемента массива
 function randomArrayElement(arr) {
   const rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
-};
+}
+// Получение опретора вычисления
+const getOperator = () => randomArrayElement(['*', '+', '-']);
+
+// Получение имени пользователя
+const getNameGlobal = () => readlineSync.question('May I have your name? ');
 // Получение случайного числа до max
 function mathRandom(max) {
   return Math.floor(Math.random() * Math.floor(max));
-};
+}
+// Получение наибольшего общего делителя
+function gcDivisor(num1, num2) {
+  let result = 1;
+  for (let i = 1; i <= num1 || i <= num2; i += 1) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      result = i;
+    }
+  }
+  return result;
+}
 // Проверка ответа пользователя к игре Brain-Even
 const checkbrainEven = (getNum, userAnswer, name) => {
   if ((getNum % 2 === 0 && userAnswer.toLowerCase() === 'yes')
@@ -22,12 +32,28 @@ const checkbrainEven = (getNum, userAnswer, name) => {
     return true;
   } if (getNum % 2 !== 0 && userAnswer.toLowerCase() === 'yes') {
     console.log(`'${userAnswer}' is wrong ;(. Correct answer is 'no'.\nLet's try again, ${name}!`);
-    return false;
   } if (getNum % 2 === 0 && userAnswer.toLowerCase() === 'no') {
     console.log(`'${userAnswer}' is wrong ;(. Correct answer is 'yes'.\nLet's try again, ${name}!`);
-    return false;
   }
 };
+// Проверка ответа пользователя к игре Brain-Calc
+const checkBrainCalc = (question, userAnswer, name) => {
+  const answer = eval(question);
+  if (answer === Number(userAnswer)) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${userAnswer}' is wrong ;(. Correct answer is ${answer}.\nLet's try again, ${name}!`);
+};
+// Проверка ответа пользователя к игре Brain-Gcd
+function checkGcd(num1, num2, userAnswer, name) {
+  const answer = gcDivisor(num1, num2);
+  if (Number(userAnswer) === answer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${userAnswer}' is wrong ;(. Correct answer is ${answer}.\nLet's try again, ${name}!`);
+}
 // Начало работы игра Brain-Even
 export const brainEven = () => {
   const name = getNameGlobal();
@@ -57,13 +83,19 @@ export const brainCalc = () => {
   }
   console.log(`Congratulations, ${name}!`);
 };
-// Проверка ответа пользователя к игре Brain-Calc
-const checkBrainCalc = (question, userAnswer, name) => {
-  if (eval(question) === Number(userAnswer)) {
-    console.log('Correct!');
-    return true;
-  } if (eval(question) !== Number(userAnswer)) {
-    console.log(`'${userAnswer}' is wrong ;(. Correct answer is ${eval(question)}.\nLet's try again, ${name}!`);
-    return false;
+// Начало работы игра brain-gcd
+export const brainGcd = () => {
+  const name = getNameGlobal();
+  console.log(`Hello, ${name}!\nFind the greatest common divisor of given numbers.`);
+  for (let i = 0; i !== 3;) {
+    const num1 = mathRandom(50);
+    const num2 = mathRandom(50);
+    const question = `${num1} ${num2}`;
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (checkGcd(num1, num2, userAnswer, name)) {
+      i += 1;
+    }
   }
+  console.log(`Congratulations, ${name}!`);
 };
